f1394f647e6ad80f57af3ef7a5a331c41e2be2f4
pc-moon <pc-moon@hotmail.com>
2016-12-06 22:06:49 +0300

    test

---
+++ start.sh
@@ -0,0 +1,12 @@
+#git clone https://github.com/rappasoft/laravel-5-boilerplate.git
+sudo chmod -R 777 laravel-5-boilerplate/
+cd laravel-5-boilerplate/
+composer update
+touch database/database.sqlite
+sudo chmod -R 777 database/database.sqlite
+php -r "file_exists('.env') || copy('.env.example', '.env');"
+php artisan optimize
+php artisan key:generate
+php artisan migrate
+php artisan db:seed
+php artisan serve
